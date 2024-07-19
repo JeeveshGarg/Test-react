ca2b2522d3dcddd46bce7b2354da25ca13043e99
JeeveshGarg <jeevesh.g@udaan.com>
2024-07-19 13:25:47 +0530

    start app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Percept from '@perceptinsight/percept-js';
+
+Percept.init("fe3626deb35c0735c2eeac2531ae05418b356d42dfac7d4e0864769f4916a8e2", {
+  enableExperiment: true,
+  autoTrackRuntimeErrors: true
+});
+Percept.setUserId('USERTEST1234');
+
 function App() {
   return (
     <div className="App">
